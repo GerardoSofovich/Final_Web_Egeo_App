f8078ba04fe2b8ba822816e04e1c479c76a4f93c
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2019-06-27 18:25:14 -0300

    Add files via upload

--- Datos/GestorChoferes.cs
+++ Datos/GestorChoferes.cs
@@ -1,10 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
     public class GestorChoferes
     {
         public static List<Choferes> GetAll()
         {
-            using(EgeoEntity db = new EgeoEntity())
+            using (EgeoEntities db = new EgeoEntities())
             {
                 return db.Choferes.ToList();
             }
         }
     }
+}
